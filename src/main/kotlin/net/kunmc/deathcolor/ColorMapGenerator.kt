b2f76bfe46630ffaed9e9afe1eaf6eda14c8d97c
Kamesuta <kamesuta@gmail.com>
2022-12-03 16:17:38 +0900

    サンプル配置

--- src/main/kotlin/net/kunmc/deathcolor/ColorMapGenerator.kt
+++ src/main/kotlin/net/kunmc/deathcolor/ColorMapGenerator.kt
@@ -1,9 +1,17 @@
 package net.kunmc.deathcolor
 
 import com.google.gson.Gson
+import net.kyori.adventure.text.Component
 import org.bukkit.Material
+import org.bukkit.World
+import org.bukkit.block.BlockFace
+import org.bukkit.block.Sign
+import org.bukkit.configuration.Configuration
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.ItemFrame
+import org.bukkit.entity.LivingEntity
+import org.bukkit.inventory.ItemStack
 import java.io.FileReader
 
 /**
@@ -96,6 +104,7 @@ object ColorMapGenerator {
 
         // エンティティのテクスチャを取得
         val map = EntityType.values()
+            // 未知のエンティティは除外
             .filter { it != EntityType.UNKNOWN }
             .associate { entityType ->
                 // エンティティのバニラID
@@ -122,5 +131,141 @@ object ColorMapGenerator {
             yaml.set(key, value)
         }
         yaml.save("../block_colors/entity_to_color.yml")
+    }
+
+    /** Materialのサンプルを作成する */
+    fun generateMaterialSample(world: World, materialToColor: Configuration) {
+        // 配置する高さ
+        val height = 4
+        Material.values().forEachIndexed { index, material ->
+            // ブロックの色をconfigから取得
+            val colorName = materialToColor.getString(material.name)
+            val color = EnumColor.values().find { it.colorName == colorName }
+
+            // 配置するブロックの座標
+            val blockAt = world.getBlockAt((index % 32) * 3, height, -(index / 32) * 5)
+            // 床を色付きの羊毛にする
+            if (color != null) {
+                for (ix in -1..1) {
+                    for (iz in -1..1) {
+                        blockAt.getRelative(ix, -1, iz).setType(color.wool, false)
+                    }
+                }
+            }
+            // ブロックを配置
+            if (material.isBlock) {
+                // ブロックの場合床に埋め込んで配置
+                blockAt.getRelative(0, -1, 0).setType(material, false)
+            } else {
+                // アイテムの場合ブロックの上に額縁を置いて配置
+                world.spawn(blockAt.location, ItemFrame::class.java).apply {
+                    setItem(ItemStack(material))
+                    setFacingDirection(BlockFace.UP, true)
+                    isFixed = true
+                }
+            }
+            // 看板にブロックIDを書いて配置
+            blockAt.getRelative(0, 0, -1).apply {
+                setType(Material.OAK_SIGN, false)
+                val blockState = state as Sign
+                material.name.chunked(15).forEachIndexed { i, name ->
+                    if (i < 4) {
+                        blockState.line(i, Component.text(name))
+                    }
+                }
+                blockState.update()
+            }
+        }
+    }
+
+    /** Materialのサンプルからコンフィグを更新する */
+    fun saveMaterialSample(world: World, materialToColor: Configuration) {
+        // 配置する高さ
+        val height = 4
+        Material.values().forEachIndexed { index, material ->
+            // 配置するブロックの座標
+            val blockAt = world.getBlockAt((index % 32) * 3, height, -(index / 32) * 5)
+
+            // 上に羊毛がある場合はコンフィグを更新
+            val newColorBlock = sequence {
+                for (ix in -1..1) {
+                    for (iz in -1..1) {
+                        yield(blockAt.getRelative(ix, 0, iz))
+                    }
+                }
+            }.mapNotNull { block -> EnumColor.values().find { it.wool == block.type } }.firstOrNull()
+
+            if (newColorBlock != null) {
+                // 出力
+                println("${material.name} = $newColorBlock")
+                // ブロックの色をconfigに反映
+                materialToColor.set(material.name, newColorBlock.colorName)
+            }
+        }
+    }
+
+    /** EntityTypeのサンプルを作成する */
+    fun generateEntitySample(world: World, entityToColor: Configuration) {
+        // 配置する高さ
+        val height = 4
+        EntityType.values()
+            // 未知のエンティティは除外
+            .filter { it != EntityType.UNKNOWN }
+            // プレイヤーは除外
+            .filter { it != EntityType.PLAYER }
+            .forEachIndexed { index, entityType ->
+                // エンティティの色をconfigから取得
+                val colorName = entityToColor.getString(entityType.name)
+                val color = EnumColor.values().find { it.colorName == colorName }
+
+                // 配置するブロックの座標
+                val blockAt = world.getBlockAt((index % 16) * 3, height, 10 + (index / 16) * 5)
+                world.spawnEntity(blockAt.location, entityType).apply {
+                    setGravity(false)
+                    if (this is LivingEntity) {
+                        setAI(false)
+                        isInvulnerable = true
+                        isPersistent = true
+                    }
+                }
+                // 床を色付きの羊毛にする
+                if (color != null) {
+                    for (ix in -1..1) {
+                        for (iz in -1..1) {
+                            blockAt.getRelative(ix, -1, iz).setType(color.wool, false)
+                        }
+                    }
+                }
+            }
+    }
+
+    /** Entityのサンプルからコンフィグを更新する */
+    fun saveEntitySample(world: World, entityToColor: Configuration) {
+        // 配置する高さ
+        val height = 4
+        EntityType.values()
+            // 未知のエンティティは除外
+            .filter { it != EntityType.UNKNOWN }
+            // プレイヤーは除外
+            .filter { it != EntityType.PLAYER }
+            .forEachIndexed { index, entityType ->
+                // 配置するブロックの座標
+                val blockAt = world.getBlockAt((index % 16) * 3, height, 10 + (index / 16) * 5)
+                // 上に羊毛がある場合はコンフィグを更新
+                val newColorBlock = sequence {
+                    for (ix in -1..1) {
+                        for (iz in -1..1) {
+                            yield(blockAt.getRelative(ix, 0, iz))
+                        }
+                    }
+                }.mapNotNull { block -> EnumColor.values().find { it.wool == block.type } }.firstOrNull()
+
+                if (newColorBlock != null) {
+                    // 出力
+                    println("${entityType.name} = $newColorBlock")
+                    // エンティティの色をconfigに反映
+                    entityToColor.set(entityType.name, newColorBlock.colorName)
+                }
+            }
     }
 }
--- src/main/kotlin/net/kunmc/deathcolor/DeathColor.kt
+++ src/main/kotlin/net/kunmc/deathcolor/DeathColor.kt
@@ -1,15 +1,94 @@
 package net.kunmc.deathcolor
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 class DeathColor : JavaPlugin() {
+    /** Material→色の対応表 */
+    lateinit var materialToColor: YamlConfiguration
+
+    /** EntityType→色の対応表 */
+    lateinit var entityToColor: YamlConfiguration
+
     override fun onEnable() {
         // Plugin startup logic
-        ColorMapGenerator.generateBlockToColor()
-        ColorMapGenerator.generateEntityToColor()
+        saveDefaultConfig()
+        materialToColor = saveCustomConfig("material_to_color.yml")
+        entityToColor = saveCustomConfig("entity_to_color.yml")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
     }
+
+    /** コンフィグファイルを読み込む、なければresourcesからコピーする */
+    private fun saveCustomConfig(name: String): YamlConfiguration {
+        val file = File(dataFolder, name)
+        if (!file.exists()) {
+            saveResource(name, false)
+        }
+        return YamlConfiguration.loadConfiguration(file)
+    }
+
+    /** コマンド */
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (command.name != "deathcolor") return false
+
+        // 対応表をワールドに配置して調整できるコマンド
+        if (args.getOrNull(0) == "debug_generate_material_to_color") {
+            // 配置するワールド、オーバーワールドの中心に配置
+            val world = sender.server.worlds.first()
+
+            when (args.getOrNull(1)) {
+                "generateMaterial" -> {
+                    // ブロック→色の対応表を生成
+                    ColorMapGenerator.generateBlockToColor()
+                    sender.sendMessage("ブロック→色の対応表を生成しました")
+                }
+
+                "generateEntity" -> {
+                    // エンティティ→色の対応表を生成
+                    ColorMapGenerator.generateEntityToColor()
+                    sender.sendMessage("エンティティ→色の対応表を生成しました")
+                }
+
+                "material" -> {
+                    sender.sendMessage("ブロック→色のサンプルを生成します")
+                    ColorMapGenerator.generateMaterialSample(world, materialToColor)
+                    sender.sendMessage("ブロック→色のサンプルを生成しました")
+                }
+
+                "entity" -> {
+                    sender.sendMessage("エンティティ→色のサンプルを生成します")
+                    ColorMapGenerator.generateEntitySample(world, entityToColor)
+                    sender.sendMessage("エンティティ→色のサンプルを生成しました")
+                }
+
+                "saveMaterial" -> {
+                    // ブロック→色の対応表を保存
+                    ColorMapGenerator.saveMaterialSample(world, materialToColor)
+                    // コンフィグに保存
+                    materialToColor.save(File(dataFolder, "material_to_color.yml"))
+                    sender.sendMessage("ブロック→色の対応表を保存しました")
+                }
+
+                "saveEntity" -> {
+                    // エンティティ→色の対応表を保存
+                    ColorMapGenerator.saveEntitySample(world, entityToColor)
+                    // コンフィグに保存
+                    entityToColor.save(File(dataFolder, "entity_to_color.yml"))
+                    sender.sendMessage("エンティティ→色の対応表を保存しました")
+                }
+
+                else -> return false
+            }
+            return true
+        }
+
+        return false
+    }
+
 }
